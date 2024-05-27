4077ca836efc4ec65fe3e90cafe5ad6898368977
StoneCAU <seokwoo1999@gmail.com>
2024-05-28 02:50:52 +0900

    valdoes 7th mission

---
+++ UMC/spring/src/main/java/umc/spring/domain/FavoriteFood.java
@@ -0,0 +1,23 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FavoriteFood extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false, length = 10)
+    private String foodType;
+}
---
+++ UMC/spring/src/main/java/umc/spring/domain/Member.java
@@ -0,0 +1,42 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+import umc.spring.domain.mapping.AssignedMission;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Date birth;
+
+    @Column(nullable = false, length = 20)
+    private String address;
+
+    private Long mission_count;
+
+    private Long point;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<AssignedMission> assignedMissions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<FavoriteFood> favoriteFoods = new ArrayList<>();
+}
